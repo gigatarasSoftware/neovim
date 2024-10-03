b6d6908aa48b70dade1b6bd0f5f7b6872464a570
vaughng <vaughn.gigataras@orionhealth.com>
2024-10-04 11:31:44 +1300

    feat: added mapping to show inline errors on dialog

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -22,6 +22,7 @@ return {
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+      vim.keymap.set('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>', {})
     end
   }
 }
