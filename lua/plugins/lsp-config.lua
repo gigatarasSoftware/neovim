27633d5f79983d4602723f3752dad8c4a1fd32e8
Vaughn Gigataras <gigataras.software@gmail.com>
2024-12-13 23:16:33 +1300

    feat: added css and html lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "ts_ls" },
+        ensure_installed = { "lua_ls", "ts_ls", "rust_analyzer", "cssls", "html" },
         automatic_installation = true
       })
     end
@@ -20,6 +20,9 @@ return {
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
       lspconfig.ts_ls.setup({})
+      lspconfig.rust_analyzer.setup({})
+      lspconfig.cssls.setup({})
+      lspconfig.html.setup({})
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
